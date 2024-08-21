562f04520686a609aae5c4bf828b1cde1996c972
Xianghao Wang <u7321615@anu.edu.au>
2024-08-21 21:46:17 +1000

    specified gadi script's storage

--- run_1_gpu_gadi.sh
+++ run_1_gpu_gadi.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
-#PBS -P p00
+#PBS -P aw81
 #PBS -q dgxa100
 #PBS -l ncpus=16
 #PBS -l ngpus=1
 #PBS -l mem=32GB
 #PBS -l walltime=00:05:00
 #PBS -l wd
+#PBS -l storage=scratch/p00
+
+__conda_setup="$('/scratch/p00/xw6261/libraries/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/scratch/p00/xw6261/libraries/miniconda3/etc/profile.d/conda.sh" ]; then
+        . "/scratch/p00/xw6261/libraries/miniconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/scratch/p00/xw6261/libraries/miniconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
 
 legate --profile --cpus 16 \
     --gpus 4 --sysmem 100000 \
     --fbmem 39000 \
     --eager-alloc-percentage 10 \
     --mem-usage matmul.py
+
