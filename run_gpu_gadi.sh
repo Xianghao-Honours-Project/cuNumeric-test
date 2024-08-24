b15d8033558ca12e273dd4c67e06cbdd39560192
Xianghao Wang <u7321615@anu.edu.au>
2024-08-24 20:15:50 +1000

    updated the script for running on different number of gpus

---
+++ run_gpu_gadi.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#PBS -P es56
+#PBS -q dgxa100
+#PBS -l ncpus=64
+#PBS -l ngpus=8
+#PBS -l mem=256GB
+#PBS -l walltime=00:10:00
+#PBS -l wd
+#PBS -l storage=scratch/p00
+#PBS -j oe
+
+source $HOME/.bashrc
+
+conda activate legate
+
+for ngpus in 1 2 4 6 8
+do
+	echo "########## run on ${ngpus} ########"
+	
+	legate --profile --cpus 16 \
+    		--gpus 4 --sysmem 100000 \
+    		--fbmem 39000 \
+    		--eager-alloc-percentage 10 \
+    		--mem-usage matmul.py
+done
